22957fb2e9b44dd70bf7bf70a673418159211732
João Nascimento <jn.marcos@outlook.com>
2017-11-16 03:19:02 -0200

    RoundRobin finalizado ou isso
    Criação de TempoProcessamento (possui infos básicas)
    Método RoundRobin implementado

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,8 @@ import java.util.Random;
 import java.util.Set;
 
 import algoritmos.FIFO;
+import algoritmos.RoundRobin;
+import algoritmos.SJF;
 import classes_basicas.ES;
 import classes_basicas.Pagina;
 import classes_basicas.Requisicao;
@@ -24,6 +26,10 @@ public class Main {
 	public static int tempo;
 	static Random addTempo = new Random();
 
+	static FIFO fifo = new FIFO();
+	static RoundRobin rb = new RoundRobin();
+	static SJF sjf = new SJF();
+
 	/**
 	 * @param args
 	 */
@@ -31,24 +37,45 @@ public class Main {
 		//entrada-saída
 		ES io = new ES();
 		io.ler();
-		
+
+
+		//0 = fifo, 1 = lru
+		gerenciarMemoria();
+		gerenciarProcessos(0);
+	}
+
+	public static void gerenciarProcessos(int numAlgoritmo) {
+		switch(numAlgoritmo) {
+		case 0:
+			rb.algoritmo();
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		default:
+
+		}
+	}
+
+	public static void gerenciarMemoria() {
 		//apenas para testar o algoritmo
-		int numAlgoritmo = 0;
 		int numPaginas;
-		
-		FIFO fifo = new FIFO();
-		
+
+
 		boolean paginaJaAdicionada;
+
+
 		for (Requisicao req: gm.getRequisicoes()) {
 			numPaginas = (int) Math.round((double) (req.getTamanhoRequisicao()) / gm.getConfiguracoes().getTamanhoPagina());
 			System.out.println("Num pages: " + numPaginas);
 			for (int i = 0; i < numPaginas; i++) {
 				tempo = tempo + addTempo.nextInt(100);
 				System.out.println("Requisicao:" +  req.getNomeProcesso() + i);
-				
+
 				Set<Integer> paginasSorteadas = new HashSet<>();
 				paginaJaAdicionada = false;
-				
+
 				int numDaPagina;
 				do {
 					numDaPagina = gm.gerarNumeroAleatorio();
@@ -59,33 +86,27 @@ public class Main {
 						paginaJaAdicionada = true;
 						break;
 					}						
-					
+
 
 				} while(paginasSorteadas.size() < gm.getConfiguracoes().getQtdPaginas());
 				System.out.println("Qtd page sorteadas: " + paginasSorteadas.size());
 				//se chegou até aqui, é pq todas as páginas foram sorteadas. Memória cheia
 				//usar algoritmo de substituição
-				
+
 				if (!paginaJaAdicionada) {
-					switch(numAlgoritmo) {
-						case 0:
-							fifo.algoritmo(req);
-							break;
-						default:
-								
-					}
-				}
-				
-				for (int j = 0; j < gm.getPaginas().length; j++) {
-					System.out.println(gm.getPaginas()[j].getNomeProcesso());
-					System.out.println(gm.getPaginas()[j].getInstTempoArmazenada());
-					System.out.println(gm.getPaginas()[j].getInstTempoReferenciada());
-					System.out.println(gm.getPaginas()[j].isReferenciado());
-					System.out.println(gm.getPaginas()[j].isModificado());
+					fifo.algoritmo(req);
+					//outro algoritmo aqui?
+
 				}
 			}
 
+			for (int j = 0; j < gm.getPaginas().length; j++) {
+				System.out.println(gm.getPaginas()[j].getNomeProcesso());
+				System.out.println(gm.getPaginas()[j].getInstTempoArmazenada());
+				System.out.println(gm.getPaginas()[j].getInstTempoReferenciada());
+				System.out.println(gm.getPaginas()[j].isReferenciado());
+				System.out.println(gm.getPaginas()[j].isModificado());
+			}
 		}
 	}
-
 }
